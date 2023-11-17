3028cabf19a725d4d5ebd5a865c87b70b9c647ae
FelipeMDantas <felipemdantas@terra.com.br>
2023-11-16 23:42:32 -0300

    Main page further developed.

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,38 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
-      <section>
+      <section className="text-center my-16">
         <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            At our pizzeria, we blend tradition with innovation, offering a
+            diverse menu that caters to both the classic pizza connoisseur and
+            the adventurous palate seeking bold and unique combinations.
+          </p>
+          <p>
+            Savor the delight of handpicked toppings, including premium
+            pepperoni, fresh vegetables, and fine cheeses, creating a mosaic of
+            taste in every bite
+          </p>
+        </div>
       </section>
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don\t hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a
+            href="tel:+46738123123"
+            className="text-4xl underline text-gray-500"
+          >
+            +46 738 123 123
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2023 All rights reserved
+      </footer>
     </>
   );
 }
