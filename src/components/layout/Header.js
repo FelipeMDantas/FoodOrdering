e68427f01996c878748b8c8de645192ccfe111dd
FelipeMDantas <felipemdantas@terra.com.br>
2023-11-17 23:52:47 -0300

    Register page created.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,17 +4,20 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between">
       <nav className="flex gap-8 items-center text-gray-500 font-semibold">
-        <Link href="" className="text-primary font-semibold text-2xl">
+        <Link href="/" className="text-primary font-semibold text-2xl">
           ST PIZZA
         </Link>
-        <Link href="">Home</Link>
+        <Link href="/">Home</Link>
         <Link href="">Menu</Link>
         <Link href="">About</Link>
         <Link href="">Contact</Link>
       </nav>
       <nav className="flex items-center gap-4 text-gray-500 font-semibold">
-        <Link href="">Login</Link>
-        <Link href="" className="bg-primary rounded-full text-white px-8 py-2">
+        <Link href="/login">Login</Link>
+        <Link
+          href="/register"
+          className="bg-primary rounded-full text-white px-8 py-2"
+        >
           Register
         </Link>
       </nav>
