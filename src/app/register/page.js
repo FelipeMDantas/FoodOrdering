dbc523d5e21ce154d858d9d7236ec62ab88c0519
FelipeMDantas <felipemdantas@terra.com.br>
2023-11-22 23:36:48 -0300

    Database and User model created. Connection with MongoDB established.

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -5,14 +5,18 @@ import { useState } from "react";
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [creatingUser, setCreatingUser] = useState(true);
+  const [userCreated, setUserCreated] = useState(false);
 
   function handleFormSubmit(e) {
     e.preventDefault();
+    setCreatingUser(true);
     fetch("/api/register", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
+    setCreatingUser(false);
   }
 
   return (
@@ -24,14 +28,18 @@ const RegisterPage = () => {
           placeholder="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={creatingUser}
         />
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={creatingUser}
         />
-        <button type="submit">Register</button>
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
         <div className="my-4 text-center text-gray-500">
           or login with provider
         </div>
