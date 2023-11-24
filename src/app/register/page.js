0388bca2aeb78a366977df8f9e545950a2afe058
FelipeMDantas <felipemdantas@terra.com.br>
2023-11-23 23:48:16 -0300

    Registration functionality coded.

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,27 +1,52 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [creatingUser, setCreatingUser] = useState(true);
+  const [creatingUser, setCreatingUser] = useState(false);
   const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
 
-  function handleFormSubmit(e) {
+  async function handleFormSubmit(e) {
     e.preventDefault();
     setCreatingUser(true);
-    fetch("/api/register", {
+    setError(false);
+    setUserCreated(false);
+
+    const response = await fetch("/api/register", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
+    if (response.ok) {
+      setUserCreated(true);
+    } else {
+      setError(true);
+    }
     setCreatingUser(false);
   }
 
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created.
+          <br /> Now you can{" "}
+          <Link href={"/login"} className="underline">
+            Login &raquo;
+          </Link>
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center">
+          An error has occured.
+          <br /> Please try again later.
+        </div>
+      )}
       <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
         <input
           type="email"
@@ -47,6 +72,12 @@ const RegisterPage = () => {
           <Image src={"/google.png"} alt="" width={24} height={24} />
           Login with Google
         </button>
+        <div className="text-center my-4 text-gray-500 border-t pt-4">
+          Existing account?{" "}
+          <Link href={"/login"} className="underline">
+            Login here &raquo;
+          </Link>
+        </div>
       </form>
     </section>
   );
