30b960ca93f296e489ed804820bedef8feb959f1
FelipeMDantas <felipemdantas@terra.com.br>
2023-11-26 18:44:32 -0300

    Profile update functionality coded.

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -3,13 +3,49 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { redirect } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ProfilePage = () => {
   const session = useSession();
   const { status } = session;
 
-  const [userName, setUserName] = useState(session?.data?.user?.name || "");
+  const [userName, setUserName] = useState("");
+  const [saved, setSaved] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+
+  useEffect(() => {
+    if (status === "authenticated") setUserName(session.data.user.name);
+  }, [session, status]);
+
+  async function handleProfileInfoUpdate(e) {
+    e.preventDefault();
+    setSaved(false);
+    setIsSaving(true);
+    const response = await fetch("/api/profile", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: userName }),
+    });
+    setIsSaving(false);
+
+    if (response.ok) {
+      setSaved(true);
+    }
+  }
+
+  async function handleFileChange(e) {
+    const files = e.files;
+
+    if (files?.length > 0) {
+      const data = new FormData();
+      data.set("files", files);
+      await fetch("/api/upload", {
+        method: "POST",
+        body: data,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    }
+  }
 
   if (status === "loading") return "Loading...";
 
@@ -20,6 +56,16 @@ const ProfilePage = () => {
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
       <div className="max-w-md mx-auto">
+        {saved && (
+          <h2 className="text-center bg-green-100 p-4 rounded-lg border border-green-300">
+            Profile saved!
+          </h2>
+        )}
+        {isSaving && (
+          <h2 className="text-center bg-blue-100 p-4 rounded-lg border border-blue-300">
+            Saving!
+          </h2>
+        )}
         <div className="flex gap-4 items-center">
           <div className="p-2 rounded-lg relative">
             <Image
@@ -29,10 +75,19 @@ const ProfilePage = () => {
               alt="avatar"
               className="rounded-lg w-full h-full mb-1"
             />
-            <button type="button">Edit</button>
+            <label>
+              <input
+                type="file"
+                className="hidden"
+                onChange={handleFileChange}
+              />
+              <span className="block border border-gray-300 rounded-lg p-2 text-center cursor-pointer">
+                Edit
+              </span>
+            </label>
           </div>
 
-          <div className="grow">
+          <form className="grow" onSubmit={handleProfileInfoUpdate}>
             <input
               type="text"
               placeholder="First and last name"
@@ -45,7 +100,7 @@ const ProfilePage = () => {
               value={session.data.user.email}
             />
             <button type="submit">Save</button>
-          </div>
+          </form>
         </div>
       </div>
     </section>
--- src/models/User.js
+++ src/models/User.js
@@ -1,22 +1,15 @@
 import { Schema, model, models } from "mongoose";
-import bcrypt from "bcrypt";
 
 const UserSchema = new Schema(
   {
+    name: { type: String },
     email: { type: String, required: true, unique: true },
     password: {
       type: String,
       required: true,
-      min: [5, "Passwords must be at least 5 characters long."],
     },
   },
   { timestamps: true }
 );
-
-UserSchema.post("validate", function (user) {
-  const notHashedPassword = user.password;
-  const salt = bcrypt.genSaltSync(10);
-  user.password = bcrypt.hashSync(notHashedPassword, salt);
-});
 
 export const User = models?.User || model("User", UserSchema);
