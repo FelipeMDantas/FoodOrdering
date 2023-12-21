fac96fec737912676225ee1e1101721a5d2b6a81
FelipeMDantas <felipemdantas@terra.com.br>
2023-12-20 23:35:26 -0300

    AppContext structured.

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,3 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import SectionHeaders from "@/components/layout/SectionHeaders";
+import MenuItem from "@/components/menu/MenuItem";
+
 export default function MenuPage() {
-  return <section>Menu here</section>;
+  const [categories, setCategories] = useState([]);
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/categories").then((res) => {
+      res.json().then((categories) => setCategories(categories));
+    });
+
+    fetch("/api/menu-items").then((res) => {
+      res.json().then((categories) => setMenuItems(menuItems));
+    });
+  }, []);
+
+  return (
+    <section className="mt-8">
+      {categories?.length > 0 &&
+        categories.map((c) => (
+          <div>
+            <div className="text-center">
+              <SectionHeaders mainHeader={c.name} />
+            </div>
+            <div className="grid grid-cols-3 gap-4 mt-6 mb-12">
+              {menuItems
+                .filter((m) => m.category === c._id)
+                .map((item) => {
+                  <MenuItem {...item} />;
+                })}
+            </div>
+          </div>
+        ))}
+    </section>
+  );
 }
--- src/components/context/AppContext.js
+++ src/components/context/AppContext.js
@@ -1,8 +1,28 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
+import { createContext, useState } from "react";
+
+const CartContext = createContext({});
 
 const AppProvider = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  const [cartProducts, setCartProducts] = useState([]);
+
+  function addToCart(product, size = null, extras = []) {
+    setCartProducts((prevProducts) => {
+      const cartProduct = { ...product, size, extras };
+      const newProducts = [...prevProducts, cartProduct];
+
+      return newProducts;
+    });
+  }
+
+  return (
+    <SessionProvider>
+      <CartContext.Provider value={{ cartProducts, setCartProducts }}>
+        {children}
+      </CartContext.Provider>
+    </SessionProvider>
+  );
 };
 
 export default AppProvider;
